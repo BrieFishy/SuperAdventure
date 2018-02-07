43719a80e35af69923158c56bfc31522af214fc4
Jennifer Rutherford <jenniferedenruther@gmail.com>
2018-02-07 14:41:35 -0700

    Added shop class but it still has errors with assigning _buyItem in SuperAdventure.cs

---
+++ Engine/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Shop : Location
+    {
+        public readonly List<Item> forSale = new List<Item>();
+
+        public Shop(int id, string name, string description, List<Item> ForSale) : base(id, name, description)
+        {
+            forSale = ForSale;
+        }
+
+        public Item ItemByID(int id)
+        {
+            foreach (Item item in forSale)
+            {
+                if (item.ID == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+    }
+}
